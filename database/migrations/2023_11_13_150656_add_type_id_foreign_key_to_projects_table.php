95485416c83fa463d844ecdafeb1298b8b0510cc
lorenzo-neri <lorenzo21neri@gmail.com>
2023-11-13 17:13:29 +0100

    add connection to models Type and Project

--- database/migrations/2023_11_13_150656_add_type_id_foreign_key_to_projects_table.php
+++ database/migrations/2023_11_13_150656_add_type_id_foreign_key_to_projects_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
+            $table->foreign('type_id')->references('id')->on('types');
         });
     }
 
@@ -22,7 +23,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropForeign('projects_type_id_foreign');
+            $table->dropColumn('type_id');
         });
     }
 };
